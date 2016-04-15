3c5a02fa76beb463a6ab46203d9433a3e090e230
Sergio Gutierrez <sergio@gokarumi.com>
2016-04-15 19:54:09 +0200

    Moooore preparation

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Karumi',
     author_email='tech@karumi.com',
     url='https://github.com/Karumi/Kin',
-    download_url='https://github.com/Karumi/Kin/releases/tag/0.9.0',
+    download_url='https://github.com/Karumi/Kin/tarball/0.9.0',
     keywords=['xcode', 'project.pbxproj', 'lint', 'objective-c',
               'swift', 'iOS'],
     classifiers=[]
