0722eebdb9762fede9c85513bec7ecaa2f3df5f5
Sergio Gutierrez <sergio@gokarumi.com>
2016-04-19 23:44:54 +0200

    Prepare for release 1.0.0

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='kin',
-    version='1.0.0.dev25',
+    version='1.0.0',
     description='A simple project.pbxproj verifier',
     long_description=long_description,
     url='https://github.com/Karumi/Kin',
