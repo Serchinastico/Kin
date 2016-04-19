e9b87633cfc57938f067c0f64d97d70e2fb0c705
Serchinastico <54Cymru@gmail.com>
2016-04-20 00:15:52 +0200

    Auto detect project (#8)
    * Auto detect project.pbxproj file if no argument provided
    * Correct README and update help message

--- README.rst
+++ README.rst
@@ -8,6 +8,7 @@ Usage
 
 ::
 
-  Usage: kin yourproject.pbxproj
+  Usage: kin [project.pbxproj]
 
-  Verifies the correctness of your project.pbxproj file
+  Verifies the correctness of your project.pbxproj file.
+  When called with no arguments, Kin will try to find a project.pbxproj file in the current project.
--- kin/kin.py
+++ kin/kin.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import sys
+import os
 from verifier import Verifier
 from grammar.PBXProjLexer import PBXProjLexer
 from grammar.PBXProjParser import PBXProjParser
@@ -16,16 +17,32 @@ def verify_file(file):
         print("CORRECT")
 
 
+def find_target_file():
+    for f in os.listdir(os.getcwd()):
+        if f.endswith(".xcodeproj"):
+            return os.path.join(f, "project.pbxproj")
+    return None
+
+
 def print_help():
-    print("Usage: kin yourproject.pbxproj\n\
+    print("Usage: kin [project.pbxproj]\n\
 \n\
-Verifies the correctness of your project.pbxproj file")
+Verifies the correctness of your project.pbxproj file.\n\
+When called with no arguments, Kin will try to find a project.pbxproj \
+file in the current project")
 
 
 def main():
     if len(sys.argv) == 2:
-        verify_file(sys.argv[1])
+        target = sys.argv[1]
     else:
+        target = find_target_file()
+
+    if target:
+        verify_file(target)
+    else:
+        print("ERROR: Unable to find project.pbxproj \
+file in the current directory\n")
         print_help()
 
 
