1dc05b2d3cbcf2fd908709e996e083d3d1d973d3
Sergio Gutierrez <sergio@gokarumi.com>
2016-04-20 09:23:30 +0200

    Return error code when there is an error
    * Return error code -1 if kin finds at least one error in the target
      file
    * Return error code -2 if kin cannot find any project.pbxproj

--- kin/kin.py
+++ kin/kin.py
@@ -7,9 +7,43 @@ from grammar.PBXProjLexer import PBXProjLexer
 from grammar.PBXProjParser import PBXProjParser
 
 
-def verify_file(file):
+def check_errors(file):
     verifier = Verifier()
     errors = verifier.check_errors(file)
+    return errors
+
+
+def find_target_file():
+    target = None
+
+    if len(sys.argv) == 2:
+        target = sys.argv[1]
+    else:
+        for f in os.listdir(os.getcwd()):
+            if f.endswith(".xcodeproj"):
+                target = os.path.join(f, "project.pbxproj")
+
+    return target
+
+
+def assert_valid_target_file(target):
+    if not target:
+        print("ERROR: Unable to find project.pbxproj \
+file in the current directory\n")
+        print("Usage: kin [project.pbxproj]\n\
+\n\
+Verifies the correctness of your project.pbxproj file.\n\
+When called with no arguments, Kin will try to find a project.pbxproj \
+file in the current project")
+        sys.exit(-2)
+
+
+def assert_no_errors(errors):
+    if errors:
+        sys.exit(-1)
+
+
+def print_verification_result(errors):
     if errors:
         for error in errors:
             print("ERROR: " + error)
@@ -17,33 +51,12 @@ def verify_file(file):
         print("CORRECT")
 
 
-def find_target_file():
-    for f in os.listdir(os.getcwd()):
-        if f.endswith(".xcodeproj"):
-            return os.path.join(f, "project.pbxproj")
-    return None
-
-
-def print_help():
-    print("Usage: kin [project.pbxproj]\n\
-\n\
-Verifies the correctness of your project.pbxproj file.\n\
-When called with no arguments, Kin will try to find a project.pbxproj \
-file in the current project")
-
-
 def main():
-    if len(sys.argv) == 2:
-        target = sys.argv[1]
-    else:
-        target = find_target_file()
-
-    if target:
-        verify_file(target)
-    else:
-        print("ERROR: Unable to find project.pbxproj \
-file in the current directory\n")
-        print_help()
+    target = find_target_file()
+    assert_valid_target_file(target)
+    errors = check_errors(target)
+    print_verification_result(errors)
+    assert_no_errors(errors)
 
 
 if __name__ == '__main__':
