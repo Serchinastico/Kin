d112188be615e5b7968c159fa9b6c79294e19c4a
Sergio Gutierrez <sergio@gokarumi.com>
2016-12-12 18:34:05 +0100

    Prepare for release 1.6.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='kin',
-    version='1.5.0',
+    version='1.6.0',
     description='A simple project.pbxproj verifier',
     long_description=long_description,
     url='https://github.com/Karumi/Kin',
