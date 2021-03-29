93fad5adfeabc01107abf514637a69e4a9de95ef
Davide Mendolia <davide@gokarumi.com>
2021-03-29 17:48:50 +0200

    update version number

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='kin',
-    version='1.7.4',
+    version='2.0.1',
     description='A simple project.pbxproj verifier',
     long_description=long_description,
     url='https://github.com/Karumi/Kin',
