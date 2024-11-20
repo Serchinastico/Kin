4c55b0eed75d072d85af728e34b7fdcdf6d6e266
Serchinastico <github@serchinastico.com>
2024-11-20 01:24:01 +0100

    Add makefile to the project to make it easier to work on the tool

--- kin/grammar/PBXProjParser.py
+++ kin/grammar/PBXProjParser.py
@@ -1,4 +1,4 @@
-# Generated from PBXProj.g4 by ANTLR 4.13.1
+# Generated from PBXProj.g4 by ANTLR 4.13.2
 # encoding: utf-8
 from antlr4 import *
 from io import StringIO
@@ -1339,7 +1339,7 @@ class PBXProjParser ( Parser ):
 
     def __init__(self, input:TokenStream, output:TextIO = sys.stdout):
         super().__init__(input, output)
-        self.checkVersion("4.13.1")
+        self.checkVersion("4.13.2")
         self._interp = ParserATNSimulator(self, self.atn, self.decisionsToDFA, self.sharedContextCache)
         self._predicates = None
 
